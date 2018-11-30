19f9e629d44d84e35252228f19cf12124367573a
jjak0b <32840785+jjak0b@users.noreply.github.com>
2018-11-30 17:05:59 +0100

    Iniziata implementazione del Main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "utility.h"
+#include "list_char.h"
 
-int main(){
+list_c_handler *assembler( list_c_handler *lh_input ){
+	list_c_handler *lh_output = NULL;
+	node_char *node_current = lh_input->head;
+	while( node_current != NULL ){
+		// do stuff
+		node_current = next(lh_input);
+	}
+	return lh_output;
+}
+
+int main( int nArgs, char **args ){
+	char *filename = args[1];
+
+	list_c_handler *lh_input = NULL, *lh_output = NULL;
+	if( filename == NULL ){
+		printf("FIle input non speficato\n");
+	}
+	else if( !readFile( filename, lh_input ) ){
+		printf("Impossbile aprire il file '%s'", filename );
+	}
+	else{
+		// lh_output = assembler( lh_input );
+
+	}
 	return 0;
 }
+
