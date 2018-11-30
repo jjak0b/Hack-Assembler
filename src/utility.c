7519383af690c17e32be3d0e21793650d1bf57ec
jjak0b <32840785+jjak0b@users.noreply.github.com>
2018-11-30 17:05:18 +0100

    Aggiunta utility per leggere da un file ed inserire in una lista

--- src/utility.c
+++ src/utility.c
@@ -1,1 +1,33 @@
+#include "utility.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "list_char.h"
+
+/**
+ * @brief
+ * PreCondition: N/A
+ * PostCondition: dato il nome di un file.estensione come stringa e un list_c_handler,
+ * inserisce nella coda i caratteri letti dal file specificato
+ * @param *filename 
+ * @param *list_handler
+ * @return 0 se è impossbile aprire il file, 1 altrimenti
+ */
+int readFile( char *filename, list_c_handler *list_handler ){
+    FILE *fin;
+    char c = "\0";
+    fin = fopen( filename, "r");
+    if( fin != NULL){
+        while( !feof( fin ) ){
+            fscanf( fin, "%c", &c);
+            list_handler = enqueue( list_handler, c );
+        }
+        fclose( fin );
+    }
+    else{
+        return false;
+    }
+    return true;
+}
+
+int extractFilename( char *filename, char *returnBuffer){
+}
--- src/utility.h
+++ src/utility.h
@@ -1,5 +1,16 @@
-
+#include "list_char.h"
 
 /*Definizione di "booleani" true e false anche se saranno considerati come interi*/
 #define true 1
 #define false 0
+
+/**
+ * @brief
+ * PreCondition: list_handler != NULL
+ * PostCondition: dato il nome di un file.estensione come stringa e un list_c_handler,
+ * inserisce nella coda i caratteri letti dal file specificato
+ * @param *filename 
+ * @param *list_handler
+ * @return 0 se è impossbile aprire il file altrimenti 1
+ */
+int readFile( char *filename, list_c_handler *list_handler );
