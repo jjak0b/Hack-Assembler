3e974b0ceb2f6e093c3f6ee4622a941dfbd68acb
jjak0b <32840785+jjak0b@users.noreply.github.com>
2018-12-01 00:25:17 +0100

    Implementata lettura e scrittura su e da una lista, controllo estensioni

--- src/main.c
+++ src/main.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include "utility.h"
 #include "list_char.h"
+#include <string.h>
+
+#define FILE_INPUT_EXTENSION ".asm" // estensione personalizzabile
+#ifndef FILE_INPUT_EXTENSION
+#define FILE_INPUT_EXTENSION ".asm"
+#endif
+#define FILE_OUTPUT_EXTENSION ".hack" // estensione personalizzabile
+#ifndef FILE_OUTPUT_EXTENSION
+#define FILE_OUTPUT_EXTENSION ".hack"
+#endif
 
 list_c_handler *assembler( list_c_handler *lh_input ){
 	list_c_handler *lh_output = NULL;
@@ -14,19 +24,56 @@ list_c_handler *assembler( list_c_handler *lh_input ){
 }
 
 int main( int nArgs, char **args ){
-	char *filename = args[1];
 
-	list_c_handler *lh_input = NULL, *lh_output = NULL;
-	if( filename == NULL ){
-		printf("FIle input non speficato\n");
-	}
-	else if( !readFile( filename, lh_input ) ){
-		printf("Impossbile aprire il file '%s'", filename );
+	if( nArgs > 1 && nArgs < 3){
+		char *filename = args[1];
+		list_c_handler *lh_input = NULL, *lh_output = NULL;
+		
+		if( filename == NULL ){
+			printf("ERRORE: File input non speficato\n");
+		}
+		else{
+			if( !strEndWith( filename, FILE_INPUT_EXTENSION ) ){
+				printf("ERRORE: Estensione file non supportata\n");
+			}
+			else{
+				lh_input = readFile( filename, lh_input );
+				if( lh_input == NULL ){
+					printf("ERRORE: Impossbile aprire il file '%s'\n", filename );
+				}
+				else if( lh_input->head != NULL && lh_input->head->value == '\0'){
+					printf("ERRORE: Il file '%s' risulta vuoto\n", filename );
+				}
+				else{
+					lh_output = lh_input; // testing copia contenuto
+					printf("file '%s' letto\n", filename);
+					printf("caratteri letti: %d\n", size( lh_output->head, true ) );
+					
+					// lh_output = assembler( lh_input ); // elabora il contenuto del file, restituendo il contenuto da scrivere su file
+					node_char_print( lh_output->head );
+
+
+					int length_estension = strlen( FILE_OUTPUT_EXTENSION );
+					int length_filename = strlen( filename );
+					int length_FilenameOut = length_filename + length_estension; // la dimensione non è proprio ottimizzata ma sicuramente non andrà outofbound
+					char *filename_out = ( char* ) malloc(sizeof(char) * ( length_FilenameOut + 1 ) );
+					strncpy( filename_out, filename, length_FilenameOut );
+					replaceFilenameExtension( filename_out, length_filename, FILE_OUTPUT_EXTENSION );
+					printf("Scrittura elaborazione su file '%s' in corso...\n", filename_out);
+					if( writeFile( filename_out, lh_input) ){
+						printf("Scrittura sul file '%s' avvenuta con successo\n", filename_out );
+					}
+					else{
+						printf("ERRORE: Impossibile aprire o scrivere sul file '%s'\n", filename_out );
+					}
+				}
+			}
+		}
 	}
 	else{
-		// lh_output = assembler( lh_input );
-
+		printf("ERRORE: File input mancante, paramteri trovati: %d\n", nArgs);
 	}
+
 	return 0;
 }
 
