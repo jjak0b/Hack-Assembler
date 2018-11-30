1d432fcbbad4f7006f23de1bf1fa224751fd4f51
jjak0b <32840785+jjak0b@users.noreply.github.com>
2018-12-01 00:23:01 +0100

    Aggiunta e modifiche di funzioni per nomi di file, definizione tipo bool

--- src/utility.c
+++ src/utility.c
@@ -1,7 +1,24 @@
 #include "utility.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "list_char.h"
+
+bool strEndWith( char *str, char *str_suffix){
+
+	int length_str = strlen( str ) - 1;
+	int length_suffix = strlen( str_suffix ) - 1;
+	int isRight = true;
+
+	while( length_suffix >= 0 && length_str >= 0 && isRight){
+		if( str[ length_str ] != str_suffix[ length_suffix ] ){
+			isRight = false;
+		}
+		length_str -= 1;
+		length_suffix -= 1;
+	}
+	return isRight;
+}
 
 /**
  * @brief
@@ -10,18 +27,48 @@
  * inserisce nella coda i caratteri letti dal file specificato
  * @param *filename 
  * @param *list_handler
- * @return 0 se è impossbile aprire il file, 1 altrimenti
+ * @return NULL se è impossbile aprire il file, un handler della nuova lista altrimenti
  */
-int readFile( char *filename, list_c_handler *list_handler ){
+list_c_handler *readFile( char *filename, list_c_handler *list_handler ){
     FILE *fin;
-    char c = "\0";
+    char c = '\0';
     fin = fopen( filename, "r");
     if( fin != NULL){
         while( !feof( fin ) ){
-            fscanf( fin, "%c", &c);
-            list_handler = enqueue( list_handler, c );
+			if( fscanf( fin, "%c", &c) == 1 ){
+				printf("c_:%c\n", c);
+				list_handler = enqueue( list_handler, c );
+			}
         }
         fclose( fin );
+    }
+    else{
+        return NULL;
+    }
+
+    return list_handler;
+}
+
+/**
+ * @brief 
+ * PreCondition: N/A
+ * PostCondition: Dato il nome di un file.estensione come stringa e un list_c_handler,
+ * scrive gli elementi della lista in un file con il nome indicato ( se non esite, viene creato )
+ * @param filename 
+ * @param list_handler 
+ * @return int 0 se è impossbile aprire/scrivere il file, 1 altrimenti
+ */
+bool writeFile( char *filename, list_c_handler *list_handler ){
+	FILE *fout;
+    fout = fopen( filename, "w");
+    if( fout != NULL){
+		list_handler->current = list_handler->head;
+		node_char *current = list_handler->current;
+        while( !feof( fout ) && current != NULL){
+			fprintf( fout, "%c", current->value );
+			current = next( list_handler);
+        }
+        fclose( fout );
     }
     else{
         return false;
@@ -29,5 +76,17 @@ int readFile( char *filename, list_c_handler *list_handler ){
     return true;
 }
 
-int extractFilename( char *filename, char *returnBuffer){
+void *replaceFilenameExtension( char filename[], int size_filename, const char extension[] ){
+	int length = strnlen( filename, size_filename );
+	int i = length ;
+	bool b_quit = false;
+	while( i >= 0 && !b_quit){
+		if( filename[ i ] == '.' ){
+			filename[ i ] = '\0';
+			b_quit = true;			
+		}		
+		i-= 1;
+	}
+
+	strncat( filename, extension, strlen(extension) );
 }
--- src/utility.h
+++ src/utility.h
@@ -1,16 +1,51 @@
-#include "list_char.h"
+#ifndef UTILITY_H
+#define UTILITY_H
 
+/*
+	Per usare meno spazio occupato da una variabile che io considero essere booleana,
+	potrei considerare 'unsigned char' di 1 solo byte per usare il booleano (memorizzando comunque interi) ma non vorrei essere frainteso
+	e che venga considerato come errore ...
+*/
+typedef unsigned short int bool; 
 /*Definizione di "booleani" true e false anche se saranno considerati come interi*/
 #define true 1
 #define false 0
 
+#include "list_char.h"
+
+/**
+ * @brief 
+ * PreCondition: str e str_suffix devono essere già stringhe definite con "\0"
+ * PostCondition: ritorna 1 se  la stringa str termina con str_suffix
+ * 	es: strEndWith("la mia stringa", "stringa") -> 1
+ * @param str 
+ * @param str_suffix 
+ * @return 1 (true) se la stringa str termina con str_suffix 
+ * @return 0 (false) altrimenti
+ */
+bool strEndWith( char *str, char *str_suffix);
+
 /**
  * @brief
- * PreCondition: list_handler != NULL
+ * 
+ * @param filename 
+ * @param size dimensione array di str_filename
+ * @param extension stringa che definisce il nome della nuova estensione ( incluso il punto )
+ * @return int 
+ */
+void *replaceFilenameExtension( char *filename, int size_filename, const char *extension );
+
+/**
+ * @brief
+ * PreCondition: N/A
  * PostCondition: dato il nome di un file.estensione come stringa e un list_c_handler,
  * inserisce nella coda i caratteri letti dal file specificato
  * @param *filename 
  * @param *list_handler
- * @return 0 se è impossbile aprire il file altrimenti 1
+ * @return NULL se è impossbile aprire il file, un handler della nuova lista altrimenti
  */
-int readFile( char *filename, list_c_handler *list_handler );
+list_c_handler *readFile( char *filename, list_c_handler *list_handler );
+
+bool writeFile( char *filename, list_c_handler *list_handler );
+
+#endif
